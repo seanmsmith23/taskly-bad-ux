518ae9b7fbceb1b1cdf06af4a2a1e3129be92a4c
Sean Smith <seansmith1020@gmail.com>
2014-08-11 15:43:36 -0600

    hamster easing and button moving

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,22 @@
 $(function() {
   $( ".datepicker" ).datepicker();
 });
+
+$(document).ready(function(){
+  $('.complete-button').mouseover(function(){
+    $(this).toggleClass('something');
+  });
+  $('.delete-button').mouseover(function(){
+    $(this).toggleClass('something-2');
+  });
+  $('.filter-button').mouseover(function(){
+    $(this).toggleClass('something-3');
+  });
+  $('.search-button').mouseover(function(){
+    $(this).toggleClass('something-3');
+  });
+  $('.logout').mouseover(function(){
+    $(this).toggleClass('something-4');
+    $('.hamster').toggleClass('hamster-fill').fadeIn('slow');
+  });
+});
